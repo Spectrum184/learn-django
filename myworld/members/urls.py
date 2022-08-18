d30eaa9555e309c42e2dbc627f91659b30dbe0d6
Thanh Nguyen <nvthanh1804@gmail.com>
2022-08-18 19:07:31 +0900

    second commit

--- myworld/members/urls.py
+++ myworld/members/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('add-member/', views.add_member, name='add-member'),
+    path('add-member/create-member/', views.create_member, name='create-member')
 ]
--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from .models import Members
+
 
 def index(request):
-    return HttpResponse("Hello World!")
+    template = loader.get_template('members.html')
+    my_members = Members.objects.all().values()
 
-# Create your views here.
+    context = {
+        "members": my_members
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def add_member(request):
+    template = loader.get_template('add-member.html')
+
+    return HttpResponse(template.render({}, request))
+
+
+def create_member(request):
+    first_name = request.POST['first']
+    last_name = request.POST['last']
+
+    print(f"first name {first_name}, last name {last_name}")
+
+    member = Members(first_name=first_name, last_name=last_name)
+    member.save()
+
+    return HttpResponseRedirect(reverse('index'))
