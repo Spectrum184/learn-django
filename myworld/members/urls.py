9c7b7951a76103deb0f61e27b7335bfef212405b
Thanh Nguyen <nvthanh1804@gmail.com>
2022-08-18 22:00:54 +0900

    third commit

--- myworld/members/urls.py
+++ myworld/members/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add-member/', views.add_member, name='add-member'),
-    path('add-member/create-member/', views.create_member, name='create-member')
+    path('add-member/create-member/', views.create_member, name='create-member'),
+    path('delete/<int:id>', views.delete_member, name='delete-member'),
+    path('update/<int:id>', views.update_member, name='update-member'),
+    path('update/update-member/<int:id>', views.save_member, name='update-member')
 ]
--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,3 +1,5 @@
+from re import template
+from urllib import request
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
@@ -25,9 +27,32 @@ def create_member(request):
     first_name = request.POST['first']
     last_name = request.POST['last']
 
-    print(f"first name {first_name}, last name {last_name}")
-
     member = Members(first_name=first_name, last_name=last_name)
     member.save()
 
     return HttpResponseRedirect(reverse('index'))
+
+def delete_member(request, id):
+    member = Members.objects.get(id=id)
+    member.delete()
+    
+    return HttpResponseRedirect(reverse('index'))
+def update_member(request, id):
+    member = Members.objects.get(id=id)
+    template = loader.get_template('update-member.html')
+    context = {
+        "member" : member
+    }
+    
+    return HttpResponse(template.render(context, request))
+
+def save_member(request, id):
+    first_name = request.POST["first"]
+    last_name = request.POST["last"]
+    
+    member = Members.objects.get(id=id)
+    member.first_name = first_name
+    member.last_name = last_name
+    member.save()
+    
+    return HttpResponseRedirect(reverse("index"))
